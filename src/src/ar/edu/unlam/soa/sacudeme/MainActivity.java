d0b9637463d77825d972d709442bc29822de2db0
EduRomero <eduardoromero91@gmail.com>
2014-04-27 16:56:30 -0300

    Agrego la base del sonido

--- src/src/ar/edu/unlam/soa/sacudeme/MainActivity.java
+++ src/src/ar/edu/unlam/soa/sacudeme/MainActivity.java
@@ -1,29 +1,50 @@
 package ar.edu.unlam.soa.sacudeme;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+	Button b1;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		b1 = (Button) findViewById(R.id.button1);
+		
+		createListeners();
+		
 		if (savedInstanceState == null) {
 			getFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
 	}
 
+	private void createListeners() {
+		b1.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				startSecondActivity(1);
+			}
+		});
+	}
+	
+	private void startSecondActivity(int buttonNum) {
+		Intent intent = new Intent(this, SecondActivity.class);
+		intent.putExtra("BUTTON NUMBER", buttonNum);
+		startActivity(intent);
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
