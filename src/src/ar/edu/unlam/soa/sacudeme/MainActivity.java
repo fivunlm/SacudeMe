b7343ddad44e313997e8b467eee2ac26f0f4fddd
flopez <lopez.fernando.damian@gmail.com>
2014-04-27 15:33:35 -0300

    Se agrega la deteccion de la acceleracion en los tres ejes.

--- src/src/ar/edu/unlam/soa/sacudeme/MainActivity.java
+++ src/src/ar/edu/unlam/soa/sacudeme/MainActivity.java
@@ -3,15 +3,29 @@ package ar.edu.unlam.soa.sacudeme;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.os.Build;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements SensorEventListener {
+
+	private SensorManager mSensorManager = null;
+	private Sensor mAccelerometer = null;
+	private TextView mTxtX = null;
+	private TextView mTxtY = null;
+	private TextView mTxtZ = null;
+	private float mLastX, mLastY, mLastZ;
+	private boolean mInitialized = false;
+	private float NOISE = 2;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +36,15 @@ public class MainActivity extends Activity {
 			getFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
+
+		mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+		mAccelerometer = mSensorManager
+				.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+		mSensorManager.registerListener(this, mAccelerometer,
+				SensorManager.SENSOR_DELAY_NORMAL);
+
+		
 	}
 
 	@Override
@@ -59,6 +82,63 @@ public class MainActivity extends Activity {
 					false);
 			return rootView;
 		}
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		mSensorManager.registerListener(this, mAccelerometer,
+				SensorManager.SENSOR_DELAY_NORMAL);
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		mSensorManager.unregisterListener(this);
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		mTxtX = (TextView) findViewById(R.id.txtX);
+		mTxtY = (TextView) findViewById(R.id.txtY);
+		mTxtZ = (TextView) findViewById(R.id.txtZ);
+		float x = event.values[0];
+		float y = event.values[1];
+		float z = event.values[2];
+		
+		if (!mInitialized) {
+			mLastX = x;
+			mLastY = y;
+			mLastZ = z;
+			mTxtX.setText("0.0");
+			mTxtY.setText("0.0");
+			mTxtZ.setText("0.0");
+			mInitialized = true;
+		} else {
+			float deltaX = Math.abs(mLastX - x);
+			float deltaY = Math.abs(mLastY - y);
+			float deltaZ = Math.abs(mLastZ - z);
+			if (deltaX < NOISE)
+				deltaX = (float) 0.0;
+			if (deltaY < NOISE)
+				deltaY = (float) 0.0;
+			if (deltaZ < NOISE)
+				deltaZ = (float) 0.0;
+			mLastX = x;
+			mLastY = y;
+			mLastZ = z;
+			mTxtX.setText(Float.toString(deltaX));
+			mTxtY.setText(Float.toString(deltaY));
+			mTxtZ.setText(Float.toString(deltaZ));
+			
+		}
+
 	}
 
 }
